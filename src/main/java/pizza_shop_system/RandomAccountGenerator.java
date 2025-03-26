081345fdf471dac04a6c4b6f7b71e89d3a86d14c
Tyler S <tschaef4@students.kennesaw.edu>
2025-03-25 23:38:46 -0400

    Began basic account gen and saving to file for future affirm testing. Also have base code for credit card toggle

--- src/main/java/pizza_shop_system/RandomAccountGenerator.java
+++ src/main/java/pizza_shop_system/RandomAccountGenerator.java
@@ -1,16 +1,60 @@
 package pizza_shop_system;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
 public class RandomAccountGenerator {
+
+    private static Random random = new Random();
+
     public RandomAccountGenerator() {
     }
 
     public static void main(String[] args) {
-        RandomAccount account = new RandomAccount();
-        System.out.println("Name: " + account.name);
-        System.out.println("Email: " + account.email);
-        System.out.println("Phone Number: " + account.phoneNumber);
-        System.out.println("Address: " + account.address);
-        System.out.println("Password: " + account.password);
-        System.out.println("Account Type: " + account.accountType);
+
+        int numAccountsToGen = 1000;
+        boolean isManagement;
+        //boolean creditCard;
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("random_accounts.txt"))) {
+
+            for(int i =0; i < numAccountsToGen; i++){
+
+                //SETUP FOR CREDIT CARD TESTS NEED GEN RANDOM CC INFO FIRST
+                //creditCard = random.nextBoolean();
+                //if(creditCard) {
+                //    RandomAccount account = new RandomAccount();
+                //    writer.write("ID: " + account.getId() + ", NAME: " + account.getName() + ", EMAIL: " + account.getEmail() +
+                //            ", PHONE NUMBER: " + account.getPhoneNumber() + ", PASSWORD: " + account.getPassword() + ", ACCOUNT TYPE: "
+                //            + account.getAccountType() + ", CREDIT CARD: " + account.getCreditCard());
+                //} else {
+                //    RandomAccount account = new RandomAccount();
+                //    writer.write("ID: " + account.getId() + ", NAME: " + account.getName() + ", EMAIL: " + account.getEmail() +
+                //            ", PHONE NUMBER: " + account.getPhoneNumber() + ", PASSWORD: " + account.getPassword() + ", ACCOUNT TYPE: "
+                //            + account.getAccountType());
+                //}
+
+                isManagement = random.nextBoolean();
+                RandomAccount account = new RandomAccount();
+                if(isManagement){
+                    account.setAccountType("Management");
+                }
+                writer.write("ID: " + account.getId() + ", NAME: " + account.getName() + ", EMAIL: " + account.getEmail() +
+                        ", PHONE NUMBER: " + account.getPhoneNumber() + ", PASSWORD: " + account.getPassword() + ", ACCOUNT TYPE: "
+                        + account.getAccountType() + "\n");
+            }
+            System.out.println("1000 random accounts have been created for testing and have been saved to random_accounts.txt");
+            writer.close();
+
+        } catch (IOException e){
+            System.err.println("FILE IOException");
+            e.printStackTrace();
+        } catch (Exception e){
+            System.err.println("Error: Check the trace.");
+            e.printStackTrace();
+        }
+
     }
 }
