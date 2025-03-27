542bf923b264fe8e8327061ef87a29a4dbc46fc6
Tyler S <tschaef4@students.kennesaw.edu>
2025-03-27 19:33:26 -0400

    Slight update to generator with base to read orders.txt as well as order_items.txt

--- src/main/java/pizza_shop_system/ReportGenerator.java
+++ src/main/java/pizza_shop_system/ReportGenerator.java
@@ -1,10 +1,13 @@
 package pizza_shop_system;
 
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
+
 
 public class ReportGenerator {
 
@@ -13,7 +16,78 @@ public class ReportGenerator {
     private double amount;
     private int orderNumber;
 
-    public static void generateDailyReport(LocalDate specifiedDate){
+    public static List<String> readOrders(int orderID) throws IOException{
+
+        List<String> orders = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader("data_files/orders.txt"))) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                String[] fields = line.split(",");
+                int checkOrderID = Integer.parseInt(fields[0].trim());
+
+                if (checkOrderID == orderID) {
+                    String customerName = fields[1].trim();
+                    int customerID = Integer.parseInt(fields[2]);
+                    LocalDate date = LocalDate.parse(fields[3].trim());
+                    String orderType = fields[4].trim();
+                    String status = fields[5].trim();
+                    String paymentMethod = fields[6].trim();
+                    List<String> orderedItems = readOrderItems(orderID);
+                    System.out.println(orders);
+                }
+
+            }
+        } catch (IOException e) {
+            System.err.println("IOException: Unable to read file " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return orders;
+
+    }
+
+    public static List<String> readOrderItems(int orderID) throws IOException{
+
+        List<String> items = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader("data_files/order_items.txt"))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] fields = line.split(",");
+
+                if (Integer.parseInt(fields[0]) == orderID) {
+                    items.addAll(Arrays.asList(fields).subList(1, fields.length));
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("IOException: Unable to read file " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        //Check if list is populated if not return empty list
+        return items.isEmpty() ? new ArrayList<>() : items;
+    }
+
+    //METHOD TO READ users.txt and store account info
+    //public static Account readAccounts(String customerName, int customerID) throws IOException{
+
+    //}
+
+    public static void generateDailyReport(List<Order> Orders, LocalDate specifiedDate){
+
 
         //CHANGE AS NEEDED FOR DATABASE FILE
         //for(Order order: orders){
@@ -45,6 +119,8 @@ public class ReportGenerator {
         //}
 
     }
+
+    //Orders will have orderID, userID, orderType, status, deliveryAddress, driverID
 
     public static void main(String[] args){
 
