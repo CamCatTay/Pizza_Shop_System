00fb88dcae6e94246686eb0860dda6a2cdf4b160
camcattay <182511057+CamCatTay@users.noreply.github.com>
2025-03-30 12:33:50 -0400

    Implement main sign up functionality

---
+++ src/main/java/pizza_shop_system/SignUpHandler.java
@@ -0,0 +1,7 @@
+package pizza_shop_system;
+
+import javafx.scene.control.TextField;
+
+public class SignUpHandler {
+
+}
--- src/main/java/pizza_shop_system/SignUpSceneController.java
+++ src/main/java/pizza_shop_system/SignUpSceneController.java
@@ -1,12 +1,62 @@
 package pizza_shop_system;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 import java.io.IOException;
 
 public class SignUpSceneController {
+    private String submittedEmail;
+    private String submittedPassword;
+    private String submittedVerifyPassword;
+    private String submittedName;
+    private String submittedAddress;
+    private String submittedPhoneNumber;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private TextField verifyPasswordField;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField addressField;
+    @FXML
+    private TextField phoneNumberField;
     private final SceneController sceneController = new SceneController();
 
     public void switchToLoginScene(ActionEvent actionEvent) throws IOException {
         sceneController.switchToLoginScene(actionEvent);
+    }
+
+    @FXML
+    public void handleEmailChanged() {
+        this.submittedEmail = emailField.getText();
+    }
+    @FXML
+    public void handlePasswordChanged() {
+        this.submittedEmail = emailField.getText();
+    }
+    @FXML
+    public void handleVerifyPasswordChanged() {
+        this.submittedEmail = emailField.getText();
+    }
+    @FXML
+    public void handleNameChanged() {
+        this.submittedEmail = emailField.getText();
+    }
+    @FXML
+    public void handleAddressChanged() {
+        this.submittedEmail = emailField.getText();
+    }
+    @FXML
+    public void handlePhoneNumberChanged() {
+        this.submittedEmail = emailField.getText();
+    }
+
+    public void handleSignUpClick() {
+
     }
 }
