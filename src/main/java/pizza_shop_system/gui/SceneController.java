0bf5bbf962d0c962c91250b90e07d60ecb24c1c3
camcattay <182511057+CamCatTay@users.noreply.github.com>
2025-04-09 21:01:01 -0400

    Updated switchToCustomerMenuScene

--- src/main/java/pizza_shop_system/gui/SceneController.java
+++ src/main/java/pizza_shop_system/gui/SceneController.java
@@ -45,11 +45,17 @@ public class SceneController {
         stage.show();
     }
 
-    public void switchToCustomerMenuScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pizza_shop_system/CustomerMenuScene.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    // This method directly references the scene instead of getting it from ActionEvent (Useful if we aren't switching scenes using a GUI event).
+    public void switchToCustomerMenuScene() throws IOException {
+        Stage stage = (Stage) Stage.getWindows().stream().filter(window -> window instanceof Stage).findFirst().orElse(null);
+
+        if (stage != null) {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(SceneController.class.getResource("/pizza_shop_system/CustomerMenuScene.fxml")));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } else {
+            System.err.println("Error: No active stage found.");
+        }
     }
 }
