92d2624cebb7d02d4853f67e2b92778628177487
camcattay <182511057+CamCatTay@users.noreply.github.com>
2025-04-09 21:05:13 -0400

    Remove ActionEvent dependency

--- src/main/java/pizza_shop_system/gui/SceneController.java
+++ src/main/java/pizza_shop_system/gui/SceneController.java
@@ -13,49 +13,36 @@ public class SceneController {
     private Stage stage;
     private Scene scene;
 
-    public void switchToLoginScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pizza_shop_system/LoginScene.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void switchToSignUpScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pizza_shop_system/SignUpScene.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void switchToAccountScene(ActionEvent event) throws IOException{
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pizza_shop_system/AccountScene.fxml"))); //Change path to whatever is needed for Account
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void switchToReportGeneratorScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pizza_shop_system/ReportGeneratorScene.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    // This method directly references the scene instead of getting it from ActionEvent (Useful if we aren't switching scenes using a GUI event).
-    public void switchToCustomerMenuScene() throws IOException {
+    private void SwitchToScene(String sceneReference) throws IOException {
         Stage stage = (Stage) Stage.getWindows().stream().filter(window -> window instanceof Stage).findFirst().orElse(null);
 
         if (stage != null) {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(SceneController.class.getResource("/pizza_shop_system/CustomerMenuScene.fxml")));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(SceneController.class.getResource(sceneReference)));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
         } else {
             System.err.println("Error: No active stage found.");
         }
+    }
+    public void switchToLoginScene() throws IOException {
+        SwitchToScene("/pizza_shop_system/LoginScene.fxml");
+    }
+
+    public void switchToSignUpScene() throws IOException {
+        SwitchToScene("/pizza_shop_system/SignUpScene.fxml");
+    }
+
+    public void switchToAccountScene(ActionEvent event) throws IOException{
+        SwitchToScene("/pizza_shop_system/AccountScene.fxml");
+    }
+
+    public void switchToReportGeneratorScene(ActionEvent event) throws IOException {
+        SwitchToScene("/pizza_shop_system/ReportGeneratorScene.fxml");
+    }
+
+    // This method directly references the scene instead of getting it from ActionEvent (Useful if we aren't switching scenes using a GUI event).
+    public void switchToCustomerMenuScene() throws IOException {
+        SwitchToScene("/pizza_shop_system/CustomerMenuScene.fxml");
     }
 }
