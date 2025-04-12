fc597aa7a4daabf3fb7f0641a0906d1ff76d1462
camcattay <182511057+CamCatTay@users.noreply.github.com>
2025-04-12 14:52:45 -0400

    Remove deprecated methods

--- src/main/java/pizza_shop_system/gui/CustomerMenuSceneController.java
+++ src/main/java/pizza_shop_system/gui/CustomerMenuSceneController.java
@@ -1,20 +1,4 @@
 package pizza_shop_system.gui;
 
-import pizza_shop_system.account.ActiveUser;
-import java.io.IOException;
-
 public class CustomerMenuSceneController {
-    private final SceneController sceneController = new SceneController();
-    public void handleLogoutClicked() throws IOException {
-        ActiveUser.getInstance().setCurrentUser(null);
-        sceneController.switchToLoginScene();
-    }
-
-    public void handleManageAccountButtonClick() throws IOException {
-        sceneController.switchToManageCustomerAccountScene();
-    }
-
-    public void handleOrderMenuButtonClick() throws IOException {
-        sceneController.switchToOrderMenuScene("/pizza_shop_system/CustomerMenuScene.fxml");
-    }
 }
