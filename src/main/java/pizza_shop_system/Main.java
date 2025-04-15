cc993dfebfc399354266e92b15961718186ab8e8
camcattay <182511057+CamCatTay@users.noreply.github.com>
2025-04-15 14:35:19 -0400

    Organize into folders

--- src/main/java/pizza_shop_system/Main.java
+++ src/main/java/pizza_shop_system/Main.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader navigationBarLoader = new FXMLLoader(getClass().getResource("/pizza_shop_system/NavigationBar.fxml"));
+        FXMLLoader navigationBarLoader = new FXMLLoader(getClass().getResource("/pizza_shop_system/scenes/NavigationBar.fxml"));
         Parent navigationBar = navigationBarLoader.load();
 
         BorderPane mainLayout = new BorderPane();
@@ -26,18 +26,18 @@ public class Main extends Application {
 
         // Load all scenes upfront to avoid reloading each time (Any scene not explicitly stated here CANNOT be switched to)
         SceneController sceneController = new SceneController(mainLayout);
-        sceneController.addScene("Home", "/pizza_shop_system/Home.fxml");
-        sceneController.addScene("Menu", "/pizza_shop_system/Menu.fxml");
-        sceneController.addScene("Cart", "/pizza_shop_system/Cart.fxml");
-        sceneController.addScene("Checkout", "/pizza_shop_system/Checkout.fxml");
-        sceneController.addScene("Login", "/pizza_shop_system/Login.fxml");
-        sceneController.addScene("SignUp", "/pizza_shop_system/SignUp.fxml");
-        sceneController.addScene("CustomizePizza", "/pizza_shop_system/CustomizePizza.fxml");
-        sceneController.addScene("CustomizeBeverage", "/pizza_shop_system/CustomizeBeverage.fxml");
-        sceneController.addScene("ManagerHome", "/pizza_shop_system/ManagerHome.fxml");
-        sceneController.addScene("Reports", "/pizza_shop_system/Reports.fxml");
-        sceneController.addScene("ManageAccounts", "/pizza_shop_system/ManageAccounts.fxml");
-        sceneController.addScene("OrderCompletion", "/pizza_shop_system/OrderCompletion.fxml");
+        sceneController.addScene("Home", "/pizza_shop_system/scenes/Home.fxml");
+        sceneController.addScene("Menu", "/pizza_shop_system/scenes/Menu.fxml");
+        sceneController.addScene("Cart", "/pizza_shop_system/scenes/Cart.fxml");
+        sceneController.addScene("Checkout", "/pizza_shop_system/scenes/Checkout.fxml");
+        sceneController.addScene("Login", "/pizza_shop_system/scenes/Login.fxml");
+        sceneController.addScene("SignUp", "/pizza_shop_system/scenes/SignUp.fxml");
+        sceneController.addScene("CustomizePizza", "/pizza_shop_system/scenes/CustomizePizza.fxml");
+        sceneController.addScene("CustomizeBeverage", "/pizza_shop_system/scenes/CustomizeBeverage.fxml");
+        sceneController.addScene("ManagerHome", "/pizza_shop_system/scenes/ManagerHome.fxml");
+        sceneController.addScene("Reports", "/pizza_shop_system/scenes/Reports.fxml");
+        sceneController.addScene("ManageAccounts", "/pizza_shop_system/scenes/ManageAccounts.fxml");
+        sceneController.addScene("OrderCompletion", "/pizza_shop_system/scenes/OrderCompletion.fxml");
 
 
         // Set main SceneController
@@ -48,9 +48,9 @@ public class Main extends Application {
         sceneController.switchScene("Home"); // Set the initial scene
 
         // Attach ALL CSS stylesheets
-        scene.getStylesheets().add(getClass().getResource("/pizza_shop_system/menu.css").toExternalForm());
-        scene.getStylesheets().add(getClass().getResource("/pizza_shop_system/cart.css").toExternalForm());
-        scene.getStylesheets().add(getClass().getResource("/pizza_shop_system/navigationbar.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/pizza_shop_system/stylesheets/menu.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/pizza_shop_system/stylesheets/cart.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/pizza_shop_system/stylesheets/navigationbar.css").toExternalForm());
 
 
     }
