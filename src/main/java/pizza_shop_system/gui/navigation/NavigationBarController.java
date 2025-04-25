4703b89a3dc40c4fc821f65fef52d31b2103c99f
camcattay <182511057+CamCatTay@users.noreply.github.com>
2025-04-25 18:35:53 -0400

    create StyleUtil and fadeOnHover effect. And apply effect to navigation bar buttons

--- src/main/java/pizza_shop_system/gui/navigation/NavigationBarController.java
+++ src/main/java/pizza_shop_system/gui/navigation/NavigationBarController.java
@@ -1,6 +1,7 @@
 package pizza_shop_system.gui.navigation;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
@@ -10,6 +11,7 @@ import pizza_shop_system.account.services.AccountService;
 import pizza_shop_system.account.entities.User;
 import pizza_shop_system.gui.account.AccountMenuController;
 import pizza_shop_system.gui.base.BaseController;
+import pizza_shop_system.gui.utils.StyleUtil;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -26,6 +28,7 @@ public class NavigationBarController extends BaseController {
 
     private final AccountService accountService = new AccountService();
     private static AccountMenuController accountMenuController;
+    private final StyleUtil styleUtil = new StyleUtil();
 
     private void setHomeButtonImage() {
         ImageView imageView = new ImageView();
@@ -82,32 +85,18 @@ public class NavigationBarController extends BaseController {
             }
         });
 
-        /*
-        buttonAccount.setOnAction(_ -> {
-            try {
-                AccountService accountService = new AccountService();
-                User currentUser = accountService.getActiveUser();
-                if (currentUser != null) {
-                    String accountType = currentUser.getAccountType();
-
-                    if("manager".equals(accountType)) {
-                        sceneController.switchScene("ManagerHome");
-                    } else {
-                        sceneController.switchScene("CustomerHome"); //Need to set up scene for this
-                    }
-                }
-            } catch (IOException er) {
-                System.err.println("Error switching to Account: " + er.getMessage());
-                er.printStackTrace();
-            }
-        });
-
-         */
-
         buttonBack.setOnAction(_ -> sceneController.switchToPreviousScene());
         buttonForward.setOnAction(_ -> sceneController.switchToForwardScene());
         setHomeButtonImage();
         setBackAndFowardButtonImage();
         navigationBar.setViewOrder(-1); // Keep navbar on top of all other scenes so its drop shadow can be seen
+
+        // Apply hover fade effect to every button in navbar
+        for (Node node : navigationBar.getChildren()) {
+            if (node instanceof Button button) {
+                styleUtil.fadeButtonOnHover(button);
+            }
+        }
+
     }
 }
