2d83f201443f4e48776b07f67597b2f0dd0d8a23
camcattay <182511057+CamCatTay@users.noreply.github.com>
2025-04-26 12:53:27 -0400

    start making GenerateReceipts.fxml

---
+++ src/main/java/pizza_shop_system/gui/order/GenerateReceiptsController.java
@@ -0,0 +1,11 @@
+package pizza_shop_system.gui.order;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class GenerateReceiptsController {
+    @FXML
+    private VBox ordersContainer, receiptsContainer;
+
+
+}
